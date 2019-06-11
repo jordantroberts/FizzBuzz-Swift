aedbfd4b540fcbdc7da71814d7ad168b3318e0ba
Jordan Roberts <jordanroberts@talk21.com>
2019-06-11 17:08:45 +0100

    IsDivisibleByThree test passing

---
+++ FizzBuzz/Brain.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+class Brain {
+    
+    func isDivisibleByThree(number: Int) -> Bool {
+        return true
+    }
+    
+}
+
+
+//
+//  Brain.swift
+//  FizzBuzz
+//
+//  Created by Jordan Roberts on 11/06/2019.
+//  Copyright © 2019 Jordan Roberts. All rights reserved.
+//
--- FizzBuzzTests/BrainTests.swift
+++ FizzBuzzTests/BrainTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import FizzBuzz
 
 class BrainTests: XCTestCase {
 
@@ -18,16 +19,13 @@ class BrainTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testIsDivisibleByThree() {
+        let brain = Brain()
+        // declare a new variable, brain, an instance of our Brain class
+        let result = brain.isDivisibleByThree(number: 3)
+        // brain checks whether the number is divisible by 3
+        XCTAssertEqual(result, true)
+        // We use XCTAssertEqual to compare the result of  our test with the expected resut (in this case, true) We expect that when we ask our brain to check if 2 is divisibvle by 3, it will respond true.
     }
 
 }
