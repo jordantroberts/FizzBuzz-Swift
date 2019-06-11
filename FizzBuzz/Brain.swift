9dd2a956150d6a944559fdbe9bb20edbb874f1c7
Jordan Roberts <jordanroberts@talk21.com>
2019-06-11 20:14:41 +0100

    Moved the IsDivisibleBy logic into its own function

--- FizzBuzz/Brain.swift
+++ FizzBuzz/Brain.swift
@@ -2,6 +2,14 @@ import Foundation
 
 class Brain {
     
+    func isDivisibleBy(divisor: Int, number: Int) -> Bool {
+        if number % divisor == 0 {
+            return true
+        } else {
+            return false 
+        }
+    }
+    
     func isDivisibleByThree(number: Int) -> Bool {
         if number % 3 == 0 {
         return true
