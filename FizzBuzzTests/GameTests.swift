68d9e70554d3971565b7edc0552bbc3ad80c033a
Jordan Roberts <jordanroberts@talk21.com>
2019-06-12 18:34:35 +0100

    Created Game class

---
+++ FizzBuzzTests/GameTests.swift
@@ -0,0 +1,27 @@
+//
+//  GameTests.swift
+//  FizzBuzzTests
+//
+//  Created by Jordan Roberts on 12/06/2019.
+//  Copyright © 2019 Jordan Roberts. All rights reserved.
+//
+
+import XCTest
+@testable import FizzBuzz
+
+class GameTests: XCTestCase {
+
+    let game = Game()
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+
+  
+
+}
